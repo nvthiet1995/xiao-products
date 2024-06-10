eb5fe3c219a287ddee19c161211a4f942572c78b
TND <danh.truong@southtelecom.vn>
2024-06-10 11:46:47 +0700

    XIAO-118: create migration file

---
+++ src/main/resources/db/migration/V0_0_1_20240610104520__create-product-details-table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `product_db`.`products` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` TINYTEXT NOT NULL,
+  `description` LONGTEXT NOT NULL,
+  `left_in_stock` INT NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `created_by` VARCHAR(50) NOT NULL,
+  `updated_at` DATETIME NULL,
+  `updated_by` VARCHAR(50) NULL,
+  PRIMARY KEY (`id`))
