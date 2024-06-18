a47d074916f6cbc2e381f0de2f2eb3d9fa113783
TND <danh.truong@southtelecom.vn>
2024-06-18 11:00:32 +0700

    XIAO-118: create product update Dto, change productdetail to specifications type json

---
+++ src/main/java/com/xiao/products/dto/ProductUpdateDto.java
@@ -0,0 +1,24 @@
+package com.xiao.products.dto;
+
+import com.xiao.products.validator.CheckAllValueEmpty;
+import lombok.*;
+
+import java.util.Map;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@CheckAllValueEmpty(fields = {"name", "description", "leftInStock", "specifications"}, message = "Please enter at least one piece when updating the product")
+public class ProductUpdateDto {
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Integer leftInStock;
+
+    private Map<String, String> specifications;
+}
--- src/main/java/com/xiao/products/mapper/ProductMapper.java
+++ src/main/java/com/xiao/products/mapper/ProductMapper.java
@@ -1,11 +1,8 @@
 package com.xiao.products.mapper;
 
-import com.xiao.products.dto.ProductDetailDto;
 import com.xiao.products.dto.ProductDto;
 import com.xiao.products.entity.Product;
-import com.xiao.products.entity.ProductDetail;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
@@ -14,9 +11,4 @@ public interface ProductMapper {
 
     Product productDtoToProduct(ProductDto productDto);
 
-    @Mapping(target = "product", ignore = true)
-    ProductDetail productDetailToProductDetailDto(ProductDetailDto productDetailDto);
-
-    @Mapping(target = "product", ignore = true)
-    ProductDetailDto productDetailDtoToProductDetail(ProductDetail productDetail);
 }
